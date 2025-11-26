b82a1786991d1de5a81ff6a84f3587b743a41d6c
Nedyo-hub <ilyas.fariss@student.ehb.be>
2025-11-26 14:02:43 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/be/ilyas/rentalplatform/RentalplatformApplication.java
@@ -0,0 +1,13 @@
+package be.ilyas.rentalplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalplatformApplication.class, args);
+	}
+
+}
